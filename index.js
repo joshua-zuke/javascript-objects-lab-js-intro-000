f71b59048ae7662392fab7b0b1320e23efff5abd
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-03 20:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromObjectByKey(obj,key){
-  newObj = Object.assign({}, obj)
+  var newObj = Object.assign({}, obj)
   delete newObj[key]
   return newObj
 }
